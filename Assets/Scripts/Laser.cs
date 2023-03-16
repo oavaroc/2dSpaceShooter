c97588768e4b65bc6aaeebae61c8b0f8d3802706
roger moore <rogermoore06@gmail.com>
2023-03-16 17:30:06 -0500

    Finish firing a laser and cooldown

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+        if(transform.position.y > 8)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
